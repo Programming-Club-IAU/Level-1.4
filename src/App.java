063daefb1fb5eb369f253f2317408b45e11e2e0f
fami <2220002370@iau.edu.sa>
2024-03-31 22:01:14 +0300

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        Student info = new Student();
+        System.out.print("==========================================\n Welcome to The Attendence and Grading System \n Enter The Student name: ");
+        String name=input.next();
+        
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,20 @@
+class Student {
+    double value;
+
+    Student(){
+
+    }
+    Student(double value){
+
+    }
+
+    public boolean pas(double value){
+        if (value>=3.5)
+            return true;
+        else
+            return false;
+    }
+    public void display(){
+        
+    }
+}
