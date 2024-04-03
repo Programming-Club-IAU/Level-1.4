128cb56ce70143004abb44cec26e9cccea01608f
Haya <h4yathegenius@gmail.com>
2024-04-03 21:02:37 +0300

    done?

--- src/amica.java
+++ src/amica.java
@@ -1,5 +1,5 @@
-import java.nio.channels.NonReadableChannelException;
 import java.util.Scanner;
+
 public class amica {
     public static void main(String[] args) throws Exception {
         Scanner ciao = new Scanner(System.in);
@@ -8,34 +8,55 @@ public class amica {
         String name = ciao.nextLine();
         System.out.println("Enter the attendance for each subject:");
         //list of variables
-        int gradeo,prog,phys,math,chems;
+        int prog, phys, math, chems;
+        double gpa = 0;
+        boolean Success = true;
 
         System.out.print("Math: ");
         math = ciao.nextInt();
 
-        System.out.print("Physics :");
+        System.out.print("Physics: ");
         phys = ciao.nextInt();
 
-        System.out.print("Chemistry :");
+        System.out.print("Chemistry: ");
         chems = ciao.nextInt();
 
-        System.out.print("programming: ");
+        System.out.print("Programming: ");
         prog = ciao.nextInt();
 
-        if(math<44 || phys<42 || chems<40 || prog<47){
-        System.out.print("Enter excuse of absence: ");
-        String excuse = ciao.nextLine();
-        int index = excuse.indexOf("hospital");
-        if(index!=-1){
-            math++;prog++;chems++;phys++;
+        if (math < 44 || phys < 42 || chems < 40 || prog < 47) {
+            System.out.print("Enter excuse of absence: ");
+            String forSpace = ciao.nextLine();
+            String excuse = ciao.nextLine();
+            int index = excuse.indexOf("hospital");
+            if (index != -1) {
+                math++;prog++;chems++;phys++;
+                if(math < 44 || phys < 42 || chems < 40 || prog < 47){
+                    Success = false;
+                }
+            }
+            else{
+                Success = false;
+            }
+        }
+
+        System.out.print("Enter your GPA: ");
+        gpa = ciao.nextDouble();
+        if(gpa<=3.5){
+            Success = false;
         }
         
+        System.out.println("Student information:\nName"+name+
+                           "Math Attendance: "+math+" days\n" +
+                           "Physics Attendance: "+phys+" days\n" + 
+                           "Chemistry Attendance: "+chems+" days\n" + 
+                           "Programming Attendance: "+prog+" days" + 
+                           "Grade: "+gpa);
+        if(Success){
+            System.out.println("Result: Congrats! you've success");
         }
-
-
-
-
+        else{
+            System.out.println("Result: Oops. you failed");
+        }
     }
 }
-
-
