115f98acf5a4e69145f0273edf34101e18bf04b1
Haya <h4yathegenius@gmail.com>
2024-04-03 05:47:45 +0300

    Co-authored-by: Mariam Barakat <mana5x@users.noreply.github.com>

---
+++ src/amica.java
@@ -0,0 +1,19 @@
+import java.nio.channels.NonReadableChannelException;
+import java.util.Scanner;
+public class amica {
+    public static void main(String[] args) throws Exception {
+        Scanner ciao = new Scanner(System.in);
+        System.out.println("Welcome to the Attendance and Grade System!");
+        //list of variables
+        String name; int gradeo,prog,phys,math,chems;
+
+
+
+
+
+
+
+
+    }
+}
+
