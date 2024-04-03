3410c9fe23bc68c1a4bcc8676e947dd778e8467c
Haya <h4yathegenius@gmail.com>
2024-04-03 05:58:53 +0300

    OK

--- src/amica.java
+++ src/amica.java
@@ -4,8 +4,12 @@ public class amica {
     public static void main(String[] args) throws Exception {
         Scanner ciao = new Scanner(System.in);
         System.out.println("Welcome to the Attendance and Grade System!");
+        System.out.print("Enter your name dear student: ");
+        String name = ciao.nextLine();
+        System.out.println("Enter the attendance for each subject:");
         //list of variables
-        String name; int gradeo,prog,phys,math,chems;
+        int gradeo,prog,phys,math,chems;
+
 
 
 
