9d90a836e1a3377e75399053657698e6a54cf2a3
fami <2220002370@iau.edu.sa>
2024-04-06 16:47:31 +0300

    .

--- src/App.java
+++ src/App.java
@@ -6,22 +6,25 @@ public class App {
         Scanner input=new Scanner(System.in);
         Student info = new Student();
         
-        //the friendly user interface with the inputs
+        //the friendly user interface with the inputs with checking every input with the proper cheacker
         System.out.print("==========================================\n Welcome to The Attendence and Grading System \n Enter The Student name: ");
         String name=input.nextLine();
         System.out.print("Enter The Attendence for Each of These Subjects IN ORDER (Math, Physics, Chemistry, Programming)\n>");
         int math=input.nextInt();
+        info.mathChecker(math);
         System.out.print(">");
         int physics=input.nextInt();
         System.out.print(">");
         int chemistry=input.nextInt();
         System.out.print(">");
         int programming=input.nextInt();
-        //System.out.println("Mark attendance for each subject (enter excuse fo absence)");
+        System.out.println("Mark attendance for each subject (enter excuse fo absence)");
+        String excuse=input.nextLine();
+        
         System.out.print("Enter grade for the semester (out of 5): ");
         double grade=input.nextDouble();
-        attcheacker(math,physics,chemistry,programming,grade);
-        
+        info.gradeChecker(grade);
+
         //re-enter the values to the student class (after checking with the method attcheacker)
         info.name=name;
         info.AttMath=math;
@@ -32,24 +35,5 @@ public class App {
         info.pas(grade);
         info.display();
 
-    }
-
-    public static void attcheacker(int m,int py, int c, int pg, double g){
-        if(m<0 ||m>=50){
-            System.out.println("The Math attendences is wrong enter again");
-            System.exit(0);
-        } else if(py<0 ||py>=50){
-            System.out.println("The Physic attendences is wrong enter again");
-            System.exit(0);
-        } else if(c<0 ||c>=50){
-            System.out.println("The Chemistry attendences is wrong enter again");
-            System.exit(0);
-        } else if(pg<0 ||pg>=50){
-            System.out.println("The Programming attendences is wrong enter again");
-            System.exit(0);
-        } else if(g<0.0 ||g>5.0){
-            System.out.println("The Grade input is wrong enter again");
-            System.exit(0);
-        }
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -5,6 +5,7 @@ class Student {
     int AttPhysics;
     int AttChemistry;
     int AttProgramming;
+    String excuse;
 
     Student(){
         this.name="null";
@@ -14,6 +15,7 @@ class Student {
         this.AttChemistry=0;
         this.AttProgramming=0;
     }
+
     Student(String name,double grade, int AttMath, int AttPhysics, int AttChemistry, int AttProgramming){
         this.name=name;
         this.AttMath=AttMath;
@@ -23,12 +25,53 @@ class Student {
     }
 
     public String pas(double grade){
-        if (grade>=3.5){
-            if (AttPhysics>=)
+        if (grade>=3.5 && AttMath>=44 && AttPhysics>=42 && AttChemistry>=40 && AttProgramming>=47){
+            return "Success";
+        }else
+            return "Fail";
+    }
+
+    public void display(){
+        System.out.println("\nStudent information:\nName: "+name+"\nMath Attendance: "+AttMath+" days\nPhysics Attendance: "+AttPhysics+" days\nChemistry Attendance: "+AttChemistry+" days\nProgramming Attendance: "+AttProgramming+" days\nGrade: "+grade);
+        System.out.println("\n\nThe Results: "+pas(grade));
+    }
+
+    public void mathChecker(int AttMath){
+        if(AttMath<0 || AttMath>50){
+            System.err.println("The Math Attendence is Incorrect, Please Enter Again.");
+            System.exit(0);
         }
     }
-    public void display(){
-        System.out.println("\nStudent information:\nName: "+name+"\nMath Attendance: "+AttMath+" days\nPhysics Attendance: "+AttPhysics+" days\nChemistry Attendance: "+AttChemistry+" days\nProgramming Attendance: "+AttProgramming+"days\nGrade: "+grade);
-        System.out.println("\n\nThe Results: "+pas(grade));
+
+    public void physChecker(int AttPhysics){
+        if(AttPhysics<0 || AttPhysics>50){
+            System.err.println("The Physics Attendence is Incorrect, Please Enter Again.");
+            System.exit(0);
+        }
+    }
+
+    public void chemChecker(int AttChemistry){
+        if(AttChemistry<0 || AttChemistry>50){
+            System.err.println("The Chemistry Attendence is Incorrect, Please Enter Again.");
+            System.exit(0);
+        }
+    }
+
+    public void progChecker(int AttProgramming){
+        if(AttProgramming<0 || AttProgramming>50){
+            System.err.println("The Programming Attendence is Incorrect, Please Enter Again.");
+            System.exit(0);
+        }
+    }
+
+    public void gradeChecker(double grade){
+        if(grade<0.0 || grade>5.0){
+            System.err.println("The Grade is Incorrect, Please Enter Again.");
+            System.exit(0);
+        }
+    }
+
+    public void excChecker(String excuse){
+        
     }
 }
