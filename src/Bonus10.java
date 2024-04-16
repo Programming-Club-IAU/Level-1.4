2031a96f70db06a8226844f3fd955fc8b808d729
Haya <h4yathegenius@gmail.com>
2024-04-16 09:25:21 +0300

    done with writing

--- src/Bonus10.java
+++ src/Bonus10.java
@@ -1,13 +1,15 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.BufferedReader;
-
+import java.io.IOException;
 public class Bonus10 {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\h4yat\\OneDrive - IMAM ABDULRAHMAN BIN FAISAL UNIVERSITY\\Desktop\\Level-1.4\\Level-1.4\\infoFile.txt"));
+        FileWriter w = new FileWriter("C:\\Users\\h4yat\\OneDrive - IMAM ABDULRAHMAN BIN FAISAL UNIVERSITY\\Desktop\\Level-1.4\\Level-1.4\\infoFile.txt",true);
         Scanner ciao = new Scanner(System.in);
         String line, name="", excuse="";
-        int prog=0, phys=0, math=0, chems=0;
+        int prog=0, phys=0, math=0, chem=0;
         double gpa = 0.0;
         boolean Success = true,need=false;
         System.out.println("Welcome to the Attendance and Grade System!\nThis is the bonus part of the task\nEvery detail will be taken from the txt file. \nEnjoy!") ;
@@ -23,7 +25,7 @@ public class Bonus10 {
                 phys = Integer.parseInt(line.substring(20).trim());
             }
             if (line.startsWith("Chemistry Attendance: ")) {
-                chems = Integer.parseInt(line.substring(22).trim());
+                chem = Integer.parseInt(line.substring(22).trim());
             }
             if (line.startsWith("Programming Attendance: ")) {
                 prog = Integer.parseInt(line.substring(24).trim());
@@ -37,12 +39,12 @@ public class Bonus10 {
         System.out.println("The attendance for each subject:");
         System.out.println("Math: " + math);
         System.out.println("Physics: " + phys);
-        System.out.println("Chemistry: " + chems);
+        System.out.println("Chemistry: " + chem);
         System.out.println("Programming: " + prog);
         System.out.println("Students' GPA: " + gpa);
         while(need){
                 line = reader.readLine(); // read a new line
-                if (math < 44 || phys < 42 || chems < 40 || prog < 47) {
+                if (math < 44 || phys < 42 || chem < 40 || prog < 47) {
                     System.out.print("The excuse of absence: ");
                     if (line.startsWith("Excuses: ")) {
                         excuse = String.valueOf(line.substring(9).trim());
@@ -50,8 +52,8 @@ public class Bonus10 {
                     }
                     int index = excuse.indexOf("hospital");
                     if (index != -1) {
-                        math++;prog++;chems++;phys++;
-                        if(math < 44 || phys < 42 || chems < 40 || prog < 47){
+                        math++;prog++;chem++;phys++;
+                        if(math < 44 || phys < 42 || chem < 40 || prog < 47){
                             Success = false;
                         }
                     }
@@ -65,13 +67,27 @@ public class Bonus10 {
                         "\nName: "+name+
                            "\nMath Attendance: "+math+" days\n" +
                            "Physics Attendance: "+phys+" days\n" + 
-                           "Chemistry Attendance: "+chems+" days\n" + 
+                           "Chemistry Attendance: "+chem+" days\n" + 
                            "Programming Attendance: "+prog+" days\n" + 
                            "Grade: "+gpa+" out of 5");
         if(Success){
+            try{
+                w.append("\nResult: Congrats! you've success >_<");
+                w.close();
+            }
+            catch(Exception ex){
+                System.out.println("Can't write in the file");
+            }
             System.out.println("Result: Congrats! you've success >_<");
         }
         else{
+            try{
+                w.append("\nResult: Oops. you failed :'(");
+                w.close();
+            }
+            catch(Exception ex){
+                System.out.println("Can't write in the file");
+            }
             System.out.println("Result: Oops. you failed :'(");
         }
     }
